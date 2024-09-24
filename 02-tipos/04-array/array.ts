292e599d058cbb8077ad4d4b302f37332be13e64
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-24 13:19:13 -0500

    Auto-commit: Actualización de código

--- 02-tipos/04-array/array.ts
+++ 02-tipos/04-array/array.ts
@@ -3,6 +3,6 @@
     const numeros: number[] = [1,2,3,4,5,6,7,8,9];
 
     console.log(numeros)
-   const identificacion: (number|string) [] = ["warre"]
+   const identificacion: (number|string) [] = ["warren"]
 
 })()
