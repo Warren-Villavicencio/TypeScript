46cef939f3683af91d3f38ed71d41f65729bb3ed
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-24 13:32:09 -0500

    Auto-commit: Actualización de código

--- 02-tipos/04-array/array-suma.js
+++ 02-tipos/04-array/array-suma.js
@@ -1,4 +1,11 @@
 "use strict";
 (() => {
-    const numeros = [1, 2, 3, 4];
+    // Creamos un array de números
+    const numbers = [1, 2, 3, 4, 5];
+    // Usamos el método reduce para sumar todos los elementos del array
+    const sum = numbers.reduce((accumulator, currentValue) => {
+        // En cada iteración, sumamos el valor actual al acumulador
+        return accumulator + currentValue;
+    }, 0); // El valor inicial del acumulador es 0
+    console.log('La suma total es:', sum);
 })();
