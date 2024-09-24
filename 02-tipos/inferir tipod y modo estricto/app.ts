b8488910d8b01c28cd6fa4c71b56427fe8b2e897
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-24 05:03:15 -0500

    Auto-commit: Actualización de código

--- 02-tipos/inferir tipod y modo estricto/app.ts
+++ 02-tipos/inferir tipod y modo estricto/app.ts
@@ -1,10 +1,10 @@
+(() => {
+    // Esta parte crea una función anónima. La flecha `=>` indica que es una función flecha.
+    // Los paréntesis vacíos `()` significan que la función no recibe ningún parámetro.
 
-(()=> {
+    const a = 10; // Declaramos una constante llamada 'a' y le asignamos el valor 10.
+    console.log(a); // Imprimimos el valor de 'a' en la consola, que será 10.
 
-    const a = 10;
-    console.log(a)
-
-})()
-
-
+})(); // Aquí invocamos inmediatamente la función que acabamos de crear.
+       // Los paréntesis adicionales al final hacen que se ejecute en este mismo momento.
 
