dba0e0b6e43ad040a4995e95f20cb8b070c53b17
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-24 13:44:58 -0500

    Auto-commit: Actualización de código

--- 02-tipos/04-array/arraysAvanzados.ts
+++ 02-tipos/04-array/arraysAvanzados.ts
@@ -13,7 +13,7 @@
 
     console.log ("ejemplo 0: forEach");
     const frutas = ["apple","strberry"];
-    frutas.forEach((st))
+    frutas.forEach((String))
 
 
 // Ejemplo 1: forEach - Iterar sobre un array y mostrar cada elemento
