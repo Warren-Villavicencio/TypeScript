2993eb45f5ff97f442c89e2b8ff49c7d908d9e77
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-24 13:47:22 -0500

    Auto-commit: Actualización de código

--- 02-tipos/04-array/arraysAvanzados.ts
+++ 02-tipos/04-array/arraysAvanzados.ts
@@ -12,7 +12,7 @@
  */
 
     console.log ("ejemplo 0: forEach");
-    const frutas = ["apple","strberry"];
+const frutas = ["apple","strberry"];
 frutas.forEach((frutas)) => {
         console.log(frutas);
     }
