a63acfe8c7abda3bf7a4fc4d73cf81885e021a3f
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-24 13:47:57 -0500

    Auto-commit: Actualización de código

--- 02-tipos/04-array/arraysAvanzados.js
+++ 02-tipos/04-array/arraysAvanzados.js
@@ -12,12 +12,13 @@
      */
     console.log("ejemplo 0: forEach");
     const frutas = ["apple", "strberry"];
-    frutas.forEach((frutas));
-    {
+    frutas.forEach((frutas) => {
         console.log(frutas);
     }
     // Ejemplo 1: forEach - Iterar sobre un array y mostrar cada elemento
-    console.log("Ejemplo 1: forEach");
+    , 
+    // Ejemplo 1: forEach - Iterar sobre un array y mostrar cada elemento
+    console.log("Ejemplo 1: forEach"));
     const numeros = [1, 2, 3, 4, 5];
     numeros.forEach((numero) => {
         console.log(numero); // Imprime cada número en la consola
