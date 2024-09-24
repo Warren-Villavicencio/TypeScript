725a5ca41023067e335b733844946788ed52647d
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-24 04:47:51 -0500

    Auto-commit: Actualización de código

--- 02-tipos/inferir tipod y modo estricto/Evitando_conflictos_de_nombres.js
+++ 02-tipos/inferir tipod y modo estricto/Evitando_conflictos_de_nombres.js
@@ -1,1 +1,6 @@
 "use strict";
+let nombre = "Juan";
+(function (nombre) {
+    console.log("Hola, " + nombre); // Imprimirá "Hola, Pedro"
+})("Pedro");
+console.log(nombre); // Imprimirá "Juan" (el valor original)
