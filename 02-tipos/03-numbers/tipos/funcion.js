0158486c3e8f004e70c8d0e408970b32499b0bfb
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-24 08:37:07 -0500

    Auto-commit: Actualización de código

--- 02-tipos/03-numbers/tipos/funcion.js
+++ 02-tipos/03-numbers/tipos/funcion.js
@@ -1,1 +1,11 @@
 "use strict";
+let superheroes = 10;
+let villains = 20;
+function compareHeroesAndVillains(heroes, villains) {
+    if (heroes < villains) {
+        return "Perdimos la batalla.";
+    }
+    else {
+        return "Ganamos la batalla!";
+    }
+}
