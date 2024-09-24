4577956db984bc135443e57994deb13370b2909f
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-24 08:37:49 -0500

    Auto-commit: Actualización de código

--- 02-tipos/03-numbers/tipos/interfaz.js
+++ 02-tipos/03-numbers/tipos/interfaz.js
@@ -1,1 +1,9 @@
 "use strict";
+// Función que toma un arreglo de héroes y devuelve un número
+function countPowers(heroes) {
+    let totalPowers = 0;
+    for (const hero of heroes) {
+        totalPowers += hero.powers.length;
+    }
+    return totalPowers;
+}
