1ee9a28bc0bacc67f56f58db1771665909bb310a
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-24 13:57:09 -0500

    Auto-commit: Actualización de código

--- 02-tipos/04-array/arraysAvanzados.js
+++ 02-tipos/04-array/arraysAvanzados.js
@@ -12,7 +12,7 @@
      */
     console.log("Ejemplo combinado: forEach");
     const lista_alumnos = [1, "warren", 2, "antonio"];
-    lista_alumnos.forEach((lista) => {
+    lista_alumnos.forEach((alumnos) => {
         console.log(lista);
     });
     console.log("ejemplo 0: forEach");
