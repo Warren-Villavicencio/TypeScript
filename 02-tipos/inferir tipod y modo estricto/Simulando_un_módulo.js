ae1dc0c59b4e35323bd579fadbd2f551e34bd43a
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-24 04:46:33 -0500

    Auto-commit: Actualización de código

--- 02-tipos/inferir tipod y modo estricto/Simulando_un_módulo.js
+++ 02-tipos/inferir tipod y modo estricto/Simulando_un_módulo.js
@@ -1,1 +1,17 @@
 "use strict";
+const miModulo = (function () {
+    let variablePrivada = "Soy privada";
+    function funcionPrivada() {
+        console.log("Función privada");
+    }
+    return {
+        variablePublica: "Soy pública",
+        funcionPublica: function () {
+            console.log("Función pública");
+            funcionPrivada();
+        }
+    };
+})();
+miModulo.funcionPublica(); // Llama a la función pública
+console.log(miModulo.variablePublica); // Acceder a la variable pública
+// No podemos acceder a variablePrivada ni a funcionPrivada desde fuera
