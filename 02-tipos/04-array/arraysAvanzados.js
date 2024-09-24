22598a5084a2a161590fe0e87621f4a52b1ab30b
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-24 13:51:44 -0500

    Auto-commit: Actualización de código

--- 02-tipos/04-array/arraysAvanzados.js
+++ 02-tipos/04-array/arraysAvanzados.js
@@ -22,10 +22,7 @@
         console.log(numero); // Imprime cada número en la consola
     });
 
-    console.log("Ejemplo combinado: forEach");
-    const lista_alumnos: (Number|String) = [1, "warren",2,"antonio"];
-    frutas.forEach((lista) => {
-        console.log(lista);
+    
 
 
 
