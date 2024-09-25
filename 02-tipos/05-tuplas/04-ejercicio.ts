d7fa2d9895defde86da5d2c65199edef6812e44e
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-25 13:05:36 -0500

    Auto-commit: Actualización de código

--- 02-tipos/05-tuplas/04-ejercicio.ts
+++ 02-tipos/05-tuplas/04-ejercicio.ts
@@ -5,23 +5,24 @@ interface Saludo {
   
   (()=>{
   
-    
-      // Función para crear un elemento HTML
-      function crearElemento(tag: string, contenido: string): HTMLElement {
-        const elemento = document.createElement(tag);
-        elemento.textContent = contenido;
-        return elemento;
-      }
-      
-      // Obtener el nombre del usuario
-      const nombre = prompt("Bienvenido, ¿cuál es tu nombre?");
-      const saludo: Saludo = { nombre, mensaje: "¡Estás listo!" } 
-      
-      // Crear un elemento de saludo
-      const saludoElemento = crearElemento("h1", `Hola, ${saludo.nombre}! ${saludo.mensaje}`);
-      
-      // Agregar el elemento al cuerpo del documento
-      document.body.appendChild(saludoElemento);
+    // Función para crear un elemento HTML
+function crearElemento(tag: string, contenido: string): HTMLElement {
+    const elemento = document.createElement(tag);
+    elemento.textContent = contenido;
+    return elemento;
+  }
+  
+  // Obtener el nombre del usuario
+  const nombre = prompt("Bienvenido, ¿cuál es tu nombre?");
+  const saludoNombre = nombre || "Invitado"; // Si nombre es null, se asigna "Invitado"
+  
+  const saludo: Saludo = { nombre: saludoNombre, mensaje: "¡Estás listo!" };
+  
+  // Crear un elemento de saludo
+  const saludoElemento = crearElemento("h1", `Hola, ${saludo.nombre}! ${saludo.mensaje}`);
+  
+  // Agregar el elemento al cuerpo del documento
+  document.body.appendChild(saludoElemento);
 
 
 
