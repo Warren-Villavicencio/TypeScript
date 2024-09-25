77f0713409a2196eda51fb26446700c08ee44bee
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-25 13:28:35 -0500

    Auto-commit: Actualización de código

--- 02-tipos/06-enums/02-ejercicio.ts
+++ 02-tipos/06-enums/02-ejercicio.ts
@@ -1,3 +1,28 @@
 (()=>{
-  
+    enum DiasSemana {
+        Lunes,
+        Martes,
+        Miércoles,
+        Jueves,
+        Viernes,
+        Sábado,
+        Domingo
+    }
+    
+    function obtenerDiasTrabajados(): DiasSemana[] {
+        const diasTrabajadosStr = prompt("Ingrese los días que trabaja (separados por coma):");
+        const diasTrabajadosArray = diasTrabajadosStr.split(',').map(diaStr => {
+            const dia = DiasSemana[diaStr.trim()];
+            if (!dia) {
+                console.error(`Día inválido: ${diaStr}`);
+                return undefined;
+            }
+            return dia;
+        }).filter(dia => dia !== undefined) as DiasSemana[];
+    
+        return diasTrabajadosArray;
+    }
+    
+    const diasQueTrabaja = obtenerDiasTrabajados();
+    console.log("Días que trabaja:", diasQueTrabaja);
 })();
--- 02-tipos/06-enums/javascript/02-ejercicio.js
+++ 02-tipos/06-enums/javascript/02-ejercicio.js
@@ -1,3 +1,27 @@
 "use strict";
 (() => {
+    let DiasSemana;
+    (function (DiasSemana) {
+        DiasSemana[DiasSemana["Lunes"] = 0] = "Lunes";
+        DiasSemana[DiasSemana["Martes"] = 1] = "Martes";
+        DiasSemana[DiasSemana["Mi\u00E9rcoles"] = 2] = "Mi\u00E9rcoles";
+        DiasSemana[DiasSemana["Jueves"] = 3] = "Jueves";
+        DiasSemana[DiasSemana["Viernes"] = 4] = "Viernes";
+        DiasSemana[DiasSemana["S\u00E1bado"] = 5] = "S\u00E1bado";
+        DiasSemana[DiasSemana["Domingo"] = 6] = "Domingo";
+    })(DiasSemana || (DiasSemana = {}));
+    function obtenerDiasTrabajados() {
+        const diasTrabajadosStr = prompt("Ingrese los días que trabaja (separados por coma):");
+        const diasTrabajadosArray = diasTrabajadosStr.split(',').map(diaStr => {
+            const dia = DiasSemana[diaStr.trim()];
+            if (!dia) {
+                console.error(`Día inválido: ${diaStr}`);
+                return undefined;
+            }
+            return dia;
+        }).filter(dia => dia !== undefined);
+        return diasTrabajadosArray;
+    }
+    const diasQueTrabaja = obtenerDiasTrabajados();
+    console.log("Días que trabaja:", diasQueTrabaja);
 })();
