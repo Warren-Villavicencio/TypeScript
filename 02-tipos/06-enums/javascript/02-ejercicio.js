a70488eb3431d470af9e7e2e944ebfb1e0d61250
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-25 13:30:38 -0500

    Auto-commit: Actualización de código

--- 02-tipos/06-enums/javascript/02-ejercicio.js
+++ 02-tipos/06-enums/javascript/02-ejercicio.js
@@ -12,14 +12,20 @@
     })(DiasSemana || (DiasSemana = {}));
     function obtenerDiasTrabajados() {
         const diasTrabajadosStr = prompt("Ingrese los días que trabaja (separados por coma):");
-        const diasTrabajadosArray = diasTrabajadosStr.split(',').map(diaStr => {
-            const dia = DiasSemana[diaStr.trim()];
+        if (!diasTrabajadosStr) {
+            return []; // Devuelve un array vacío si el usuario cancela
+        }
+        const diasTrabajadosArray = diasTrabajadosStr.split(',')
+            .map(diaStr => {
+            const diaIndex = parseInt(diaStr.trim());
+            const dia = DiasSemana[diaIndex];
             if (!dia) {
                 console.error(`Día inválido: ${diaStr}`);
                 return undefined;
             }
             return dia;
-        }).filter(dia => dia !== undefined);
+        })
+            .filter(dia => dia !== undefined);
         return diasTrabajadosArray;
     }
     const diasQueTrabaja = obtenerDiasTrabajados();
