ca91a60f6c5dbf16dbf44b01308d8b22f207a234
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-25 13:18:56 -0500

    Auto-commit: Actualización de código

--- 02-tipos/06-enums/01-ejercicio.ts
+++ 02-tipos/06-enums/01-ejercicio.ts
@@ -1,4 +1,19 @@
 (()=>{
   
+    enum DiasSemana {
+        Lunes,
+        Martes,
+        Miércoles,
+        Jueves,
+        Viernes,
+        Sábado,
+        Domingo
+    }
     
+    function esFinDeSemana(dia: DiasSemana): boolean {
+        return dia === DiasSemana.Sábado || dia === DiasSemana.Domingo;
+    }
+    
+    let hoy: DiasSemana = DiasSemana.Miércoles;
+    console.log(esFinDeSemana(hoy)); // Imprimirá false
 })();
