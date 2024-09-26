d4b29d641ebdb4e3d003d14fda815ca0f6c0c521
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 07:54:57 -0500

    Auto-commit: Actualización de código

--- 03-funciones-objetos/01-funciones-basicas/05-ejercicio.ts
+++ 03-funciones-objetos/01-funciones-basicas/05-ejercicio.ts
@@ -1,3 +1,6 @@
 (()=>{
-  
+  // MultiplicarPorDos.ts
+const multiplicarPorDos = (numero: number): number => numero * 2;
+// Esta es una forma más concisa de escribir una función.
+// Se utiliza mucho en TypeScript.
 })();
