e4812a23eec034d0f0cf8284615505cb1080c560
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 08:05:17 -0500

    Auto-commit: Actualización de código

--- 03-funciones-objetos/02-funciones-flecha/04-ejercicio.ts
+++ 03-funciones-objetos/02-funciones-flecha/04-ejercicio.ts
@@ -1,3 +1,8 @@
 (()=>{
-  
+  // SumarElementos.ts
+const sumarElementos = (numeros: number[]): number => {
+    // Función flecha que suma todos los elementos de un arreglo.
+    // Utiliza el método 'reduce' para reducir el arreglo a un solo valor.
+    return numeros.reduce((total, numero) => total + numero, 0);
+};
 })();
