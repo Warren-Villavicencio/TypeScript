56c5a3c22e4433383011f07a0277085a3ad382e4
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 06:46:19 -0500

    Auto-commit: Actualización de código

--- 02-tipos/07-void/javascript/02-ejercicio.js
+++ 02-tipos/07-void/javascript/02-ejercicio.js
@@ -1,15 +1,3 @@
 "use strict";
 (() => {
 })();
-// Función que modifica una variable global
-let contador = 0;
-function incrementarContador() {
-    contador++;
-}
-incrementarContador(); // Ahora contador tiene el valor 1
-// Función que realiza una llamada a una API (simulada)
-function enviarDatos(datos) {
-    console.log("Enviando datos:", datos);
-    // Aquí se realizaría la llamada real a la API
-}
-enviarDatos({ nombre: "Juan", edad: 30 });
