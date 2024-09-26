ef6defeebb7288dda5236b5a28805fe441526879
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 08:03:30 -0500

    Auto-commit: Actualización de código

--- 03-funciones-objetos/02-funciones-flecha/02-ejercicio.ts
+++ 03-funciones-objetos/02-funciones-flecha/02-ejercicio.ts
@@ -1,3 +1,9 @@
 (()=>{
-  
+  // PrimerosNNumeros.ts
+const primerosN = (n: number): number[] => {
+    // Función flecha que crea un arreglo con los primeros 'n' números naturales.
+    // Utiliza un arreglo vacío y el método 'fill' para inicializarlo con 'n' elementos.
+    // Luego, se utiliza 'map' para transformar cada elemento en su índice + 1.
+    return Array(n).fill(0).map((_, i) => i + 1);
+};
 })();
