ffdc8c2ddd4c858b4c7cae91d303ce0a50766ab2
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 08:17:12 -0500

    Auto-commit: Actualización de código

--- 03-funciones-objetos/04-funciones-parametors-defecto/04-ejercicio.ts
+++ 03-funciones-objetos/04-funciones-parametors-defecto/04-ejercicio.ts
@@ -1,3 +1,9 @@
 (()=>{
-  
+    function sumar(...numeros: number[]): number {
+        // Suma todos los números proporcionados.
+        return numeros.reduce((total, numero) => total + numero, 0);
+      }
+      
+      let resultado1 = sumar(1, 2, 3); // Imprime: 6
+      let resultado2 = sumar(10, 20); // Imprime: 30
 })();
