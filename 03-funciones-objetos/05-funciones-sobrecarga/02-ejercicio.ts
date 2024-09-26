b7356ca83a3bf766930185f4ca699a331cd04e01
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 08:21:06 -0500

    Auto-commit: Actualización de código

--- 03-funciones-objetos/05-funciones-sobrecarga/02-ejercicio.ts
+++ 03-funciones-objetos/05-funciones-sobrecarga/02-ejercicio.ts
@@ -1,3 +1,19 @@
 (()=>{
-  
+  // crearElementoHTML.ts
+function crearElementoHTML(tagName: string, attributes?: { [key: string]: string }): HTMLElement;
+function crearElementoHTML(tagName: string, textContent: string): HTMLElement;
+function crearElementoHTML(tagName: string, ...args: any[]): HTMLElement {
+  const element = document.createElement(tagName);
+  if (typeof args[0] === 'object') {
+    for (const key in args[0]) {
+      element.setAttribute(key, args[0][key]);
+    }
+  } else if (typeof args[0] === 'string') {
+    element.textContent = args[0];
+  }
+  return element;
+}
+
+const divConId = crearElementoHTML('div', { id: 'myDiv' });
+const parrafoConTexto = crearElementoHTML('p', 'Hola, mundo!');
 })();
