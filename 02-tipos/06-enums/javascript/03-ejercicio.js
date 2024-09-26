f20d65896bac07e49bd56b206bac27b4c8589c11
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-25 23:16:02 -0500

    Auto-commit: Actualización de código

--- 02-tipos/06-enums/javascript/03-ejercicio.js
+++ 02-tipos/06-enums/javascript/03-ejercicio.js
@@ -2,9 +2,9 @@
 (() => {
     let nivel_audio;
     (function (nivel_audio) {
-        nivel_audio[nivel_audio["bajo"] = ] = "bajo";
-        nivel_audio[nivel_audio["medio"] = void 0] = "medio";
-        nivel_audio[nivel_audio["alto"] = void 0] = "alto";
+        nivel_audio[nivel_audio["bajo"] = 10] = "bajo";
+        nivel_audio[nivel_audio["medio"] = 11] = "medio";
+        nivel_audio[nivel_audio["alto"] = 12] = "alto";
     })(nivel_audio || (nivel_audio = {}));
     const volumen_actual = nivel_audio.alto;
     console.log(volumen_actual);
