058279356d6fa22842651220d0675ea8402d4d31
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 08:10:17 -0500

    Auto-commit: Actualización de código

--- 03-funciones-objetos/03-funciones-parametros-opcionales/02-ejercicio.ts
+++ 03-funciones-objetos/03-funciones-parametros-opcionales/02-ejercicio.ts
@@ -1,3 +1,7 @@
 (()=>{
-  
+    function crearUsuario(nombre: string, edad: number, correoElectronico?: string): { nombre: string, edad: number, correoElectronico?: string } {
+        // Crea un objeto de usuario con las propiedades proporcionadas.
+        // El correo electrónico es opcional.
+        return { nombre, edad, correoElectronico };
+      }
 })();
