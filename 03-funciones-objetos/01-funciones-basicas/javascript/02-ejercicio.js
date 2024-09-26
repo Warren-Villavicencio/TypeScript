f821403c7d79b6c123d49a20a7fba789cc2ef366
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 07:52:38 -0500

    Auto-commit: Actualización de código

--- 03-funciones-objetos/01-funciones-basicas/javascript/02-ejercicio.js
+++ 03-funciones-objetos/01-funciones-basicas/javascript/02-ejercicio.js
@@ -1,3 +1,10 @@
 "use strict";
 (() => {
+    // SaludarPersona.ts
+    function saludar(nombre) {
+        // Esta función crea un saludo personalizado.
+        // Recibe un nombre como parámetro de tipo cadena.
+        // Devuelve un saludo con el nombre de la persona.
+        return `¡Hola, ${nombre}!`;
+    }
 })();
