28435c796eefd4c294d5f75379d6321f74be0b8c
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 08:02:28 -0500

    Auto-commit: Actualización de código

--- 03-funciones-objetos/02-funciones-flecha/01-ejercicio.ts
+++ 03-funciones-objetos/02-funciones-flecha/01-ejercicio.ts
@@ -1,6 +1,6 @@
 (()=>{
-    const nombreDeLaFuncion = (parametro1: tipo1, parametro2: tipo2, ...) => {
-        // Cuerpo de la función
-        return valorDeRetorno;
-    }
+// DuplicarNumero.ts
+const duplicar = (numero: number) => numero * 2;
+// Función flecha que duplica un número.
+// El parámetro 'numero' se multiplica por 2 y se devuelve el resultado.
 })();
