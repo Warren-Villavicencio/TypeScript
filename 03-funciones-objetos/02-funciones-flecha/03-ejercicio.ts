c0635c5e584fc8fbc87fbe936f18abec2e6958a1
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 08:04:34 -0500

    Auto-commit: Actualización de código

--- 03-funciones-objetos/02-funciones-flecha/03-ejercicio.ts
+++ 03-funciones-objetos/02-funciones-flecha/03-ejercicio.ts
@@ -1,3 +1,8 @@
 (()=>{
-  
+  // NumerosACadena.ts
+const numerosACadena = (numeros: number[]): string => {
+    // Función flecha que convierte un arreglo de números en una cadena separada por comas.
+    // Utiliza el método 'join' para unir los elementos del arreglo en una cadena.
+    return numeros.join(', ');
+};
 })();
