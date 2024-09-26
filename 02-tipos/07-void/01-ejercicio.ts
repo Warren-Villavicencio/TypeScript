7114128ab85c6b12bef98da7e03bce8219e13ef0
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 06:52:23 -0500

    Auto-commit: Actualización de código

--- 02-tipos/07-void/01-ejercicio.ts
+++ 02-tipos/07-void/01-ejercicio.ts
@@ -2,7 +2,7 @@
   
     const nombre = prompt("dime tu nombre");
 
-    function saludar(nombre): void {
+    function saludar(nombre: string): void {
         console.log("Hola, " + nombre + "!");
     }
     
