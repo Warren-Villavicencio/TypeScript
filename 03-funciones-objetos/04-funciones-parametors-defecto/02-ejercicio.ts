06af85dc00e74e4ac513c818c956eb1f053564d7
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 08:15:24 -0500

    Auto-commit: Actualización de código

--- 03-funciones-objetos/04-funciones-parametors-defecto/02-ejercicio.ts
+++ 03-funciones-objetos/04-funciones-parametors-defecto/02-ejercicio.ts
@@ -1,3 +1,9 @@
 (()=>{
-  
+    function calcularArea(base: number, altura: number = 1) {
+        // Si no se proporciona la altura, se asume que es 1.
+        return base * altura;
+      }
+      
+      let area1 = calcularArea(5); // Imprime: 5 (base 5, altura 1 por defecto)
+      let area2 = calcularArea(3, 4); // Imprime: 12
 })();
