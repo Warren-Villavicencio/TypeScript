65ad1faa2fc2b667a275f1be3599ffdeb227e35f
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 08:11:19 -0500

    Auto-commit: Actualización de código

--- 03-funciones-objetos/03-funciones-parametros-opcionales/04.ejercicio.ts
+++ 03-funciones-objetos/03-funciones-parametros-opcionales/04.ejercicio.ts
@@ -1,3 +1,7 @@
 (()=>{
-  
+    function sumarNumeros(...numeros: number[]): number {
+        // Suma todos los números proporcionados como argumentos.
+        // Utiliza el operador rest (...) para aceptar un número variable de argumentos.
+        return numeros.reduce((total, numero) => total + numero, 0);
+      }
 })();
