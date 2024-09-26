f05d1e398807ab57c7bac44c86492885f77721df
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 07:51:06 -0500

    Auto-commit: Actualización de código

--- 03-funciones-objetos/01-funciones-basicas/01-ejercicio.ts
+++ 03-funciones-objetos/01-funciones-basicas/01-ejercicio.ts
@@ -1,3 +1,9 @@
 (()=>{
-  
+  // SumaDosNumeros.ts
+function sumar(a: number, b: number): number {
+    // Esta función suma dos números y devuelve el resultado.
+    // Los parámetros 'a' y 'b' son de tipo número.
+    // El tipo de retorno también es número.
+    return a + b;
+}
 })();
--- 03-funciones-objetos/01-funciones-basicas/javascript/01-ejercicio.js
+++ 03-funciones-objetos/01-funciones-basicas/javascript/01-ejercicio.js
@@ -1,3 +1,10 @@
 "use strict";
 (() => {
+    // SumaDosNumeros.ts
+    function sumar(a, b) {
+        // Esta función suma dos números y devuelve el resultado.
+        // Los parámetros 'a' y 'b' son de tipo número.
+        // El tipo de retorno también es número.
+        return a + b;
+    }
 })();
