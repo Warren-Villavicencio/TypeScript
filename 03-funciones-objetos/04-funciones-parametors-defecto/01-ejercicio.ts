03ad34880dceacaaea24b576f257baa3804b4b94
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 08:14:47 -0500

    Auto-commit: Actualización de código

--- 03-funciones-objetos/04-funciones-parametors-defecto/01-ejercicio.ts
+++ 03-funciones-objetos/04-funciones-parametors-defecto/01-ejercicio.ts
@@ -1,3 +1,9 @@
 (()=>{
-  
+    function saludar(nombre: string, mensaje: string = 'Hola') {
+        // Si no se proporciona un mensaje, se utiliza 'Hola' por defecto.
+        console.log(`${mensaje}, ${nombre}!`);
+      }
+      
+      saludar('Juan'); // Imprime: Hola, Juan!
+      saludar('Ana', '¡Buenos días!'); // Imprime: ¡Buenos días!, Ana!
 })();
