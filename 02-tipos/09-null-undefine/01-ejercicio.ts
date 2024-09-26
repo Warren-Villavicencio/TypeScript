edd3c36d7d4e74986ebacc40d315e6b148f5427e
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 07:10:25 -0500

    Auto-commit: Actualización de código

--- 02-tipos/09-null-undefine/01-ejercicio.ts
+++ 02-tipos/09-null-undefine/01-ejercicio.ts
@@ -1,3 +1,30 @@
 (()=>{
-  
+  // Undefined
+let nombre; // Declarada pero no inicializada, su valor es undefined
+console.log(nombre); // Imprime: undefined
+
+// Null
+let edad: number | null = null; // Indicamos que puede ser un número o null
+console.log(edad); // Imprime: null
+
+// Opcionalidad
+interface Persona {
+    nombre: string;
+    edad?: number; // Edad es opcional
+}
+
+let persona1: Persona = {
+    nombre: "Juan"
+};
+console.log(persona1.edad); // Imprime: undefined (la propiedad no existe)
+
+// Manejo de errores
+function saludar(nombre: string): string {
+    if (nombre) {
+        return `Hola, ${nombre}!`;
+    } else {
+        return "Hola, desconocido!";
+    }
+}
+console.log(saludar(null)); // Imprime: Hola, desconocido!
 })();
--- 02-tipos/09-null-undefine/javascript/01-ejercicio.js
+++ 02-tipos/09-null-undefine/javascript/01-ejercicio.js
@@ -1,3 +1,23 @@
 "use strict";
 (() => {
+    // Undefined
+    let nombre; // Declarada pero no inicializada, su valor es undefined
+    console.log(nombre); // Imprime: undefined
+    // Null
+    let edad = null; // Indicamos que puede ser un número o null
+    console.log(edad); // Imprime: null
+    let persona1 = {
+        nombre: "Juan"
+    };
+    console.log(persona1.edad); // Imprime: undefined (la propiedad no existe)
+    // Manejo de errores
+    function saludar(nombre) {
+        if (nombre) {
+            return `Hola, ${nombre}!`;
+        }
+        else {
+            return "Hola, desconocido!";
+        }
+    }
+    console.log(saludar(null)); // Imprime: Hola, desconocido!
 })();
