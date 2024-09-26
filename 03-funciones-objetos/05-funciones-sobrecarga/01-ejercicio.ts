6b12f19b89c62d2902999c45070f3766974073f3
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 08:20:27 -0500

    Auto-commit: Actualización de código

--- 03-funciones-objetos/05-funciones-sobrecarga/01-ejercicio.ts
+++ 03-funciones-objetos/05-funciones-sobrecarga/01-ejercicio.ts
@@ -1,3 +1,12 @@
 (()=>{
-  
+  // sumaNumerosOConcatenarCadenas.ts
+function sumaNumerosOConcatenarCadenas(a: number, b: number): number;
+function sumaNumerosOConcatenarCadenas(a: string, b: string): string;
+function sumaNumerosOConcatenarCadenas(a: any, b: any): any {
+  // Implementación por defecto (puede ser más específica si es necesario)
+  return a + b;
+}
+
+const resultado1 = sumaNumerosOConcatenarCadenas(1, 2); // Resultado: 3
+const resultado2 = sumaNumerosOConcatenarCadenas("Hola", " Mundo"); // Resultado: "Hola Mundo"
 })();
