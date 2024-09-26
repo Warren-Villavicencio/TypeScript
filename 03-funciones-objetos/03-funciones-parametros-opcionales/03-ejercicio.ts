e4cc1d32de3a0835433a5831c97be2e84df1b4fc
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 08:10:50 -0500

    Auto-commit: Actualización de código

--- 03-funciones-objetos/03-funciones-parametros-opcionales/03-ejercicio.ts
+++ 03-funciones-objetos/03-funciones-parametros-opcionales/03-ejercicio.ts
@@ -1,3 +1,11 @@
 (()=>{
-  
+    function generarMensaje(mensaje: string, destinatario: string, asunto?: string): string {
+        // Genera un mensaje de correo electrónico.
+        // El asunto es opcional.
+        if (asunto) {
+          return `Para: ${destinatario}\nAsunto: ${asunto}\n\n${mensaje}`;
+        } else {
+          return `Para: ${destinatario}\n\n${mensaje}`;
+        }
+      }
 })();
