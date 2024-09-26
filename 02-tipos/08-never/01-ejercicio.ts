9856e9062e6796140338e7ddf9e0c90de03203af
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 07:00:17 -0500

    Auto-commit: Actualización de código

--- 02-tipos/08-never/01-ejercicio.ts
+++ 02-tipos/08-never/01-ejercicio.ts
@@ -1,3 +1,28 @@
 (()=>{
-  
+  // Función que siempre lanza una excepción
+function error(message: string): never {
+    throw new Error(message);
+}
+
+// Bucle infinito
+function infiniteLoop(): never {
+    while (true) {}
+}
+
+// Unión de tipos exhaustiva
+type Animal = 'dog' | 'cat' | 'fish';
+
+function getAnimalSound(animal: Animal): string {
+    switch (animal) {
+        case 'dog':
+            return 'Woof!';
+        case 'cat':
+            return 'Meow!';
+        case 'fish':
+            return 'Blub!';
+        default:
+            // Este caso nunca se ejecutará porque ya hemos cubierto todos los casos posibles
+            return error('Animal desconocido');
+    }
+}
 })();
