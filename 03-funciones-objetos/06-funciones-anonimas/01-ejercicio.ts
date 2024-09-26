cccb383fe481db93f5d386e22cf6e2c0a5b4193c
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 08:25:44 -0500

    Auto-commit: Actualización de código

--- 03-funciones-objetos/06-funciones-anonimas/01-ejercicio.ts
+++ 03-funciones-objetos/06-funciones-anonimas/01-ejercicio.ts
@@ -1,3 +1,12 @@
 (()=>{
-  
+  // Función anónima que suma dos números
+const sumar = (a: number, b: number): number => {
+    // Descripción: Esta función toma dos números como entrada y devuelve su suma.
+    // Explicación: Utilizamos la sintaxis de flecha para definir una función anónima. Los parámetros 'a' y 'b' son de tipo número. El cuerpo de la función simplemente devuelve la suma de 'a' y 'b'.
+    return a + b;
+};
+
+// Uso de la función
+const resultado = sumar(5, 3);
+console.log(resultado); // Imprime: 8
 })();
