d285d055b91ec1b100721c2485f73fbfbf9ca302
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 07:53:21 -0500

    Auto-commit: Actualización de código

--- 03-funciones-objetos/01-funciones-basicas/03-ejercicio.ts
+++ 03-funciones-objetos/01-funciones-basicas/03-ejercicio.ts
@@ -1,3 +1,9 @@
 (()=>{
-  
+  // EsPar.ts
+function esPar(numero: number): boolean {
+    // Esta función determina si un número es par.
+    // Recibe un número como parámetro.
+    // Devuelve un valor booleano (true si es par, false si no).
+    return numero % 2 === 0;
+}
 })();
--- 03-funciones-objetos/01-funciones-basicas/javascript/03-ejercicio.js
+++ 03-funciones-objetos/01-funciones-basicas/javascript/03-ejercicio.js
@@ -1,3 +1,10 @@
 "use strict";
 (() => {
+    // EsPar.ts
+    function esPar(numero) {
+        // Esta función determina si un número es par.
+        // Recibe un número como parámetro.
+        // Devuelve un valor booleano (true si es par, false si no).
+        return numero % 2 === 0;
+    }
 })();
