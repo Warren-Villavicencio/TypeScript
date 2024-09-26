8f1add66e2bd823a6d057227db703c6cba70aa37
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 07:54:05 -0500

    Auto-commit: Actualización de código

--- 03-funciones-objetos/01-funciones-basicas/javascript/04-ejercicio.js
+++ 03-funciones-objetos/01-funciones-basicas/javascript/04-ejercicio.js
@@ -1,3 +1,15 @@
 "use strict";
 (() => {
+    // Factorial.ts
+    function factorial(n) {
+        // Esta función calcula el factorial de un número.
+        // Recibe un número entero positivo.
+        // Devuelve el factorial de ese número.
+        if (n === 0) {
+            return 1;
+        }
+        else {
+            return n * factorial(n - 1);
+        }
+    }
 })();
