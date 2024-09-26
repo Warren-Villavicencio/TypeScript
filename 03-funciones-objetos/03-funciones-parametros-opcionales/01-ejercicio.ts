25d5aa7f88f76943ea7f434efaf8269c935835d9
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 08:09:29 -0500

    Auto-commit: Actualización de código

--- 03-funciones-objetos/03-funciones-parametros-opcionales/01-ejercicio.ts
+++ 03-funciones-objetos/03-funciones-parametros-opcionales/01-ejercicio.ts
@@ -1,3 +1,7 @@
 (()=>{
-  
+    function calcularAreaCirculo(radio: number, pi: number = 3.1416): number {
+        // Calcula el área de un círculo dado su radio.
+        // El valor de pi es opcional y por defecto se toma como 3.1416.
+        return pi * radio * radio;
+      }
 })();
