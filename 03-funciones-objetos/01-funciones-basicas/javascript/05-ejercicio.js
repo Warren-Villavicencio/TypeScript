54626681b4bbd1b80ad0e4fb642b0702e47f76d5
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 07:54:58 -0500

    Auto-commit: Actualización de código

--- 03-funciones-objetos/01-funciones-basicas/javascript/05-ejercicio.js
+++ 03-funciones-objetos/01-funciones-basicas/javascript/05-ejercicio.js
@@ -1,3 +1,7 @@
 "use strict";
 (() => {
+    // MultiplicarPorDos.ts
+    const multiplicarPorDos = (numero) => numero * 2;
+    // Esta es una forma más concisa de escribir una función.
+    // Se utiliza mucho en TypeScript.
 })();
