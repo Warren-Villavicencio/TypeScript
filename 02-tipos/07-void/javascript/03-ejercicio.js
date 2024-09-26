ed81bd704bc4718bb849b0baf4430f4038eaa192
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-26 06:53:45 -0500

    Auto-commit: Actualización de código

--- 02-tipos/07-void/javascript/03-ejercicio.js
+++ 02-tipos/07-void/javascript/03-ejercicio.js
@@ -1,3 +1,8 @@
 "use strict";
 (() => {
+    const nombre = prompt("dime tu nombre");
+    function saludar(nombre) {
+        console.log("Hola, " + nombre + "!");
+    }
+    saludar("warren"); // Imprime "Hola, Juan!"
 })();
